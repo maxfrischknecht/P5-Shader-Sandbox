cff3605843705d8814f00a42edbea5c4227dbbd4
Max Frischknecht <max@debutdebut.com>
2020-05-23 17:40:18 +0200

    add alains modified noise example

---
+++ p5-displacement-map-shader-noise/README.md
@@ -0,0 +1,6 @@
+# P5.js Displacement Map Shader
+
+based on: [https://github.com/aferriss/p5jsShaderExamples/](https://github.com/aferriss/p5jsShaderExamples/)
+
+
+![](example.png)
---
+++ p5-displacement-map-shader-noise/sketch.js
@@ -0,0 +1,30 @@
+let myShader;
+let filter, img;
+
+function preload() {
+  // myShader = loadShader('shader/effect.vert', 'shader/effect.frag');
+  // myShader = loadShader('shader/effect.vert', 'shader/effect2.frag');
+  myShader = loadShader('shader/effect.vert', 'shader/effect3.frag');
+
+  filter = loadImage('data/filter3.png');
+  img = loadImage('data/image.jpg');
+}
+
+
+function setup() {
+  let cnv = createCanvas(windowWidth, windowHeight, WEBGL);
+  cnv.parent('sketch-holder');
+  shader(myShader);
+  background(255, 0, 0)
+}
+
+function draw() {
+  myShader.setUniform('filter', filter);
+  myShader.setUniform('img', img);
+  myShader.setUniform('amt', sin(frameCount * 0.01));
+  rect(0,0,0,0);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
